6583291e0360abd88186ae89e726315444d700f4
Jose Vallino <jvvg5@gcloud.ua.es>
2023-02-11 14:33:34 +0100

    Added implementation of the main in Program class

--- hada-p1/Program.cs
+++ hada-p1/Program.cs
@@ -10,6 +10,27 @@ namespace hada_p1
     {
         static void Main(string[] args)
         {
+            string result;
+            do
+            {
+                Console.WriteLine("¿De qué unidad partes? ¿Minutos o segundos? (m o s): ");
+                string tipe = Console.ReadLine();
+                Console.WriteLine("Dime el número a convertir: ");
+                double variable = double.Parse(Console.ReadLine());
+
+                if (tipe.Equals("m"))
+                {
+                    Console.WriteLine(HadaP1.Minutes2Seconds(variable) + " segundos.");
+                }
+                else
+                {
+                    Console.WriteLine(HadaP1.Seconds2Minutes(variable) + " minutos.");
+                }
+
+                Console.WriteLine("¿Desea continuar haciendo conversiones? (sí o no): ");
+                result = Console.ReadLine();
+            }
+            while (result.Equals("sí"));
         }
     }
 }
