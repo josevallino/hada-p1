8e16b737bfff813671a15329c7769bbe502c6fb6
Jose Vallino <jvvg5@gcloud.ua.es>
2023-02-17 14:50:44 +0100

    Added new conversor method

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -6,24 +6,27 @@ using System.Threading.Tasks;
 
 namespace hada_p1
 {
-    /// <summary>
-    /// Esta clase contiene métodos para convertir de minutos a segundos y viceversa
-    /// </summary>
     class HadaP1
     {
-        // Método que convierte de segundos a minutos a partir de un double dado como argumento
+        // Convierte de segundos a minutos
         public static double Seconds2Minutes(double s)
         {
-            if (s == 0)
+            if (s != 0)
             {
-                return 0;
+                return s / 60;
             }
-            return s / 60;
+            return 0;
         }
-        // Método que convierte de minutos a segundos a partir de un double dado como argumento
+        // Convierte de minutos a segundos
         public static double Minutes2Seconds(double m)
         {
             return m * 60;
+        }
+
+        // Convierte horas a minutos
+        public static double Hours2Minutes(double h)
+        {
+            return h * 60;
         }
     }
 }
